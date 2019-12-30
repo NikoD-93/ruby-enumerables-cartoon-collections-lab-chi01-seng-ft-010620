6a6c03505ee8cd5c26b0ea55fa860d0952e85187
Kieron Niko Dixon <nikodixon@gmail.com>
2019-12-30 01:02:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,9 @@ def summon_captain_planet(veggies)
   }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+array.any? { |word| word.size > 4
+}
 end
 
 def find_the_cheese# code an argument here
